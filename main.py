7461e023bc4f6f4345127ba4300454003a3f1c7a
hrathore1995 <hrathore@umd.edu>
2025-09-14 01:59:48 -0400

    Hello world langsmith

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
-import os
+
 
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
 
 load_dotenv()
 
 
 def main():
     print("Hello from langchain-course!")
-    print("Your OpenAI API Key is:", os.environ.get("OPENAI_API_KEY"))
+    information= """
+Narendra Damodardas Modi[a] (born 17 September 1950) is an Indian politician who has served as the prime minister of India since 2014. Modi was the chief minister of Gujarat from 2001 to 2014 and is the member of parliament (MP) for Varanasi. He is a member of the Bharatiya Janata Party (BJP) and of the Rashtriya Swayamsevak Sangh (RSS), a right-wing Hindutva paramilitary volunteer organisation. He is the longest-serving prime minister outside the Indian National Congress.
 
+Modi was born and raised in Vadnagar, Bombay State (present-day Gujarat), where he completed his secondary education. He was introduced to the RSS at the age of eight, becoming a full-time worker for the organisation in Gujarat in 1971. The RSS assigned him to the BJP in 1985, and he rose through the party hierarchy, becoming general secretary in 1998.[b] In 2001, Modi was appointed chief minister of Gujarat and elected to the legislative assembly soon after. His administration is considered complicit in the 2002 Gujarat riots[c] and has been criticised for its management of the crisis. According to official records, a little over 1,000 people were killed, three-quarters of whom were Muslim; independent sources estimated 2,000 deaths, mostly Muslim.[4] A Special Investigation Team appointed by the Supreme Court of India in 2012 found no evidence to initiate prosecution proceedings against him.[d] While his policies as chief minister were credited for encouraging economic growth, his administration was criticised for failing to significantly improve health, poverty and education indices in the state.[e]
+"""
+    summary_template= """Given the information {information}, about the person I want you to create:
+    1. A short summary
+    2. Two interesting facts about the person"""
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"],
+        template=summary_template )
+    
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o")
+    #llm = ChatOllama(model="gemma3:270m", temperature=0)
+    chain = summary_prompt_template | llm
+    response = chain.invoke({"information": information})
+    print(response.content)
 
 if __name__ == "__main__":
     main()
