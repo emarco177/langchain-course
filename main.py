8f782592c4e6bd083a4973588b82514a87836975
Eden Marco <emarco177@gmail.com>
2025-11-09 10:54:21 +0200

    langchain tavily built in tool

--- main.py
+++ main.py
@@ -5,32 +5,21 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-from tavily import TavilyClient
-
-tavily = TavilyClient()
-
-@tool
-def search(query: str) -> str:
-    """
-    Tool that searches over internet
-    Args:
-        query: The query to search for
-    Returns:
-        The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
+from langchain_tavily import TavilySearch
 
 
 llm = ChatOpenAI(model="gpt-5")
-tools = [search]
-agent = create_agent(model=llm,tools=tools)
+tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
 
 
 def main():
     print("Hello from langchain-course!")
-    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
+    result = agent.invoke(
+        {"messages": HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details?")}
+    )
     print(result)
+
 
 if __name__ == "__main__":
     main()
