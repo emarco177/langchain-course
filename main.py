6d934208dac1e5f6de4a936e0898ea19662b0fe9
2732ii2 <mo.anas123khan@gmail.com>
2025-11-30 18:19:51 +0530

    langchain - smith chain

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langchain_ollama import ChatOllama
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY")
 
@@ -18,7 +18,11 @@ def main():
         model="models/gemini-2.5-pro",  # pick a model from your list
         api_key=API_KEY
     )
-
+    # llm = ChatOllama(
+    #     temprature=1,
+    #     model="gemma3:270m",  # pick a model from your list
+    #     # api_key=API_KEY
+    # )
     information="""
         Elon Reeve Musk[b] (born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be around $500 billion.
 
