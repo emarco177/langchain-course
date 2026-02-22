25a9e0c2ad3a0b5bb460513317e7ef3928b48de9
himani <himani.bhardwaj1@ibm.com>
2026-02-22 15:42:01 +0530

    Adding Structured AgentResponse via pydantic models

--- tools/tavily_search.py
+++ tools/tavily_search.py
@@ -1,3 +1,6 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
     
 load_dotenv()
@@ -7,14 +10,25 @@ from langchain_core.messages import HumanMessage
 from langchain_ollama import ChatOllama
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Schema of the source used by the agent"""
+
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer:str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(default_factory=list, description="List of sources used to generate the answer")
+    
 # using existing tool by tavily
 llm = ChatOllama(model="llama3.2")
 tool = [TavilySearch()]
-agent = create_agent(model=llm, tools=tool)
+agent = create_agent(model=llm, tools=tool, response_format=AgentResponse)
 
 def main():
     print("Hello from LangChain!")
-    result = agent.invoke({"messages": HumanMessage(content="Search for ai engineering jobs through linkedin post in last 1 hour in bengalore india")})
+    result = agent.invoke({"messages": HumanMessage(content="Search for ai engineering jobs through linkedin in bengalore india")})
     # we need to pass dictionary containing key as "messages" to agent which is going to contain the input query
     print(result)
 
