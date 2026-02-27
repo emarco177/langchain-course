b1ce0eab21f3e57b87323b378cf9b12829ff8b85
Behzad Pirvali <behzad.pirvali@vastspace.com>
2026-02-27 07:08:54 -0800

    lang-chain-course-up-to-ch15

--- 11-LangGraph/main.py
+++ 11-LangGraph/main.py
@@ -30,7 +30,7 @@ flow.add_conditional_edges(AGENT_REASON, should_continue, {
 flow.add_edge(ACT, AGENT_REASON)
 
 app = flow.compile()
-app.get_graph().draw_mermaid_png(output_file_path="flow.png")
+app.get_graph().draw_mermaid_png(output_file_path="./11-LangGraph/flow.png")
 
 if __name__ == "__main__":
     print("Hello ReAct LangGraph with Function Calling")
