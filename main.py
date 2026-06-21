396846667f6d95aec714e2e19212093b29259f2f
Rajavardhan <rajavardhanb9@gmail.com>
2026-06-20 23:18:15 -0500

    environment setup

---
+++ main.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+# from langchain_openai import ChatOpenAI
+# llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+
+def main():
+    print("What is the capital of France?")
+
+
+if __name__ == "__main__":
+    main()
